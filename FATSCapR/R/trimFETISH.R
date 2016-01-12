63530f9489ba8b5f0de77e5ea0424c2c0ee36438
vivekbhr <bhardwaj@ie-freiburg.mpg.de>
2016-01-12 17:09:53 +0100

    roxigenized and exported functions

--- FATSCapR/R/trimFETISH.R
+++ FATSCapR/R/trimFETISH.R
@@ -3,18 +3,23 @@
 
 #' Trim the R2 of FETISH data and tag the read headers
 #'
-#' @param infile Input _R2.fastq (or _R2.fastq.gz) file.
-#' @param outfile Name of the Output fastq file.
-#' @return trimmedFile : The trimmed R2.fastq file.
+#' @param input_R1 Input _R1.fastq (or _R1.fastq.gz) file.
+#' @param input_R2 Input _R2.fastq (or _R2.fastq.gz) file.
+#' @param output_R1 Name of the Output _R1.fastq.gz file.
+#' @param output_R2 Name of the Output _R2.fastq.gz file.
+#' @return output_R1, output_R2 : The trimmed R1 and R2 .fastq.gz files.
 #' @examples
-#' trimFETISH("testdata/test_R2.fastq.gz","test_trimmed_R2.fastq")
-#'
+#' trimFETISH("testdata/test_R1.fastq.gz","testdata/test_R2.fastq.gz",
+#' 		"test_trimmed_R1.fastq.gz","test_trimmed_R2.fastq.gz")
+#' @useDynLib(FATSCapR,trimFETISH)
+#' @export
 
-trimFETISH <- function(infile,outfile){
+trimFETISH <- function(input_R1,input_R2,output_R1,output_R2){
 	message("Trimming the FETISH data")
 	# passes the arguments to the C function, which accepts two chars as double pointers
-	out <- .C("trimFETISH",as.character(infile),as.character(outfile))
+	out <- .C("trimFETISH",as.character(input_R1),as.character(input_R2),
+		  as.character(output_R1),as.character(output_R2))
 	# output is a list with input filenames
-	names(out) <- c("Input","Output")
+	#names(out) <- c("Input","Output")
 	return(out) # Just print names on the screen
 }
