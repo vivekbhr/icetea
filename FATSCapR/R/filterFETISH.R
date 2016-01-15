149e2618536ab728f624ec50b7522c3043f9ba79
vivekbhr <bhardwaj@ie-freiburg.mpg.de>
2016-01-15 19:56:09 +0100

    minimal version that doesn't filter by replicate ids

--- FATSCapR/R/filterFETISH.R
+++ FATSCapR/R/filterFETISH.R
@@ -25,32 +25,34 @@ split_BAM <- function(bamFile, index_list, outfile_list, rep_indexList = NULL, n
 	## Write a CLOSURE that return the function to search idx in readname
 	message("Creating Filtering Rules")
 	make_FilterFunc <- function(idx_name,rep_name = NULL){
+
 		function(df){
 			df$qname <- as.character(df$qname)
 			df_sep <- data.frame(idx = vapply(strsplit(df$qname, "#"), "[[", character(1), 2),
 					     stringsAsFactors = FALSE)
 			df_sep2 <- data.frame(idx = vapply(strsplit(df_sep$idx, ":"), "[[", character(1), 1),
 					      stringsAsFactors = FALSE)
-			df_sep3 <- data.frame(idx = vapply(strsplit(df_sep$idx, ":"), "[[", character(1), 3),
-					      stringsAsFactors = FALSE)
+#			df_sep3 <- data.frame(idx = vapply(strsplit(df_sep$idx, ":"), "[[", character(1), 3),
+#					      stringsAsFactors = FALSE)
 
 			## grep for idx and repIdx and combine them (AND)
 			# If the replicate index name is given, then grep that from readName and
 			# return TRUE if both idx and repIdx is present. Else, only check for idx.
 
-			grep_idx_res <- grepl(idx_name,df_sep2$idx)
-			grep_repidx_res <- ifelse( rep_name != NULL,
-						  grepl(rep_name,df_sep3$idx), TRUE )
+#			grep_idx_res <- grepl(idx_name,df_sep2$idx)
+#			grep_repidx_res = ifelse( rep_name != NULL,
+#						  grepl(rep_name,df_sep3$idx), TRUE )
 
 
-			return(grep_idx_res & grep_repidx_res)
+#			return(grep_idx_res & grep_repidx_res)
+			return(grepl(idx_name,df_sep2$idx))
 		}
 
 	}
 
 	## A function to create filterRules argument
 	make_FilterRules <- function(FilterFunc){
-			return(	FilterRules(list(FilterFunc)) )
+			return(FilterRules(list(FilterFunc)) )
 	}
 
 
