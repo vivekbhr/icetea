a0982e2bdda33f8655a59dc9a29e52a2b6b5956f
vivekbhr <bhardwaj@ie-freiburg.mpg.de>
2016-01-20 14:42:23 +0100

    splitting by replicate a seperate tool

--- FATSCapR/R/splitBAM_byRepindex.R
+++ FATSCapR/R/splitBAM_byRepindex.R
@@ -5,8 +5,7 @@
 #' Split the composite BAM file using replicate indexes
 #'
 #' @param bamFile Input BAM file
-#' @param repindex_list A list of replicate indexes for splitting (character vector)
-#' @param outfile_list A list of output file names (with order corresponding to that of repindex_list)
+#' @param outfile_prefix A prefix for output file (replicates IDs will be added as RR/YY)
 #' @param nthreads Number of threads
 #'
 #' @return Filtered files by replicate Index
@@ -15,13 +14,12 @@
 #' @examples
 #' 
 #' splitBAM_byRepindex(bamFile = "FATSCapR/tests/testthat/test_sorted.bam",
-#'  	   repindex_list = c("AA" ,"AT"),
-#'  	    	   outfile_list = c("FATSCapR/tests/testthat/test_filt3.bam",
-#'  	    	   "FATSCapR/tests/testthat/test_filt4.bam"), nthreads = 10)
+#'  	   	    	   outfile_prefix = "TEST", nthreads = 10)
+#'  	   	    	   
 
 
 
-splitBAM_byRepindex <- function(bamFile, repindex_list, outfile_list, nthreads = 1) {
+splitBAM_byRepindex <- function(bamFile, outfile_prefix, nthreads = 1) {
 	
 	## Write a CLOSURE that return the function to search idx in readname
 	message("Creating Filtering Rules")
@@ -44,7 +42,9 @@ splitBAM_byRepindex <- function(bamFile, repindex_list, outfile_list, nthreads =
 		return(FilterRules(list(FilterFunc)) )
 	}
 	
-	
+	## Splitting by PURINES (RR) or PYRIMIDINES (YY)
+	repindex_list <- list(RR = paste("AA","GG","GA","AG", sep = "|"),
+				    YY = paste("CC","TT","CT","TC", sep = "|"))
 	## Now put them together, get lists back
 	
 	filtfuncs <- lapply(repindex_list,make_FilterFunc)
@@ -52,10 +52,10 @@ splitBAM_byRepindex <- function(bamFile, repindex_list, outfile_list, nthreads =
 
 	## Filter the files in parallel
 	message("Filtering the BAM file")
-	destinations <- ifelse(rep_indexList != NULL, paste(outfile_list, rep_indexList,sep = "_"), outfile_list)
+	destinations <- paste(outfile_prefix, names(repindex_list),sep = "_")
 	
 	param = BiocParallel::MulticoreParam(workers = nthreads)
-	bplapply(seq_along(destinations), function(i, file, destinations, filtrules) {
+	BiocParallel::bplapply(seq_along(destinations), function(i, file, destinations, filtrules) {
 		filterBam(file, destinations[i], filter = filtrules[[i]])
 	}, bamFile, destinations, filtrules,
 	BPPARAM = param)
