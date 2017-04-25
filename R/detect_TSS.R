81544431613d051f31674403219653cd79f4874c
vivekbhr <bhardwaj@ie-freiburg.mpg.de>
2017-04-25 19:54:18 +0200

    detect TSS function

---
+++ R/detect_TSS.R
@@ -0,0 +1,81 @@
+
+
+#' Detection of Trancription start sites based on local enrichment
+#'
+#' @param bam.files List of bam files to use
+#' @param design A data frame with rownames = sample names and a column called 'group'
+#' 		that contains information about the sample group (see example)
+#'
+#' @param logFC A log-fold change cutoff of local enrichment to detect the TSS. For samples with
+#' 		'usual' amount of starting material and squencing depth (>=5ug starting material,
+#' 		>= 5 mil reads/sample), a cut-off of 6 fold can be used. For samples with lower
+#' 		amount of material or sequencing depth, use a lower cut-off (eg. use 2-fold for
+#' 		samples with 500ng starting material).
+#'
+#' @param restrictChr Chromosomes to restrict the analysis to.
+#'
+#' @param outfile_prefix Output name prefix for the .bed files
+#'
+#'
+#' @return .bed files containing TSS position for each group, along with a bed file for consensus
+#' 	   (union) TSS sites of all samples.
+#' @export
+#'
+#' @examples
+#'
+#'
+
+
+detect_TSS <- function(bam.files, design, logFC = 2, restrictChr, outfile_prefix) {
+
+	# Define read params
+	frag.len <- NA
+	win.width <- 10
+	param <- csaw::readParam(minq=30, forward = NULL, restrict = restrictChr)
+	regionparam <- csaw::readParam(minq=30, restrict = restrictChr)
+
+	# Count reads into sliding windows
+	data <- csaw::strandedCounts(bam.files, param=param, ext=frag.len, width=win.width, bin = TRUE)
+	colnames(data) <- rownames(design)
+	colData(data) <- c(colData(data), design)
+
+	# Get counts for 2kb local region surrounding each bin
+	surrounds <- 2000
+	neighbor <- suppressWarnings(resize(rowRanges(data), surrounds, fix = "center"))
+	wider <- csaw::regionCounts(bam.files, param = regionparam, regions = neighbor, ext = frag.len)
+	colnames(wider) <- rownames(design)
+	colData(wider) <- c(colData(wider), design)
+
+	## take out groups --> Generate filter statistics for each group (based on local enrichment)
+	filterstat <- lapply(unique(design$group), function(x){
+		stat <- csaw::filterWindows(data[, data$group == x],
+					    wider[, wider$group == x],
+					    type = "local")
+		return(stat)
+	})
+
+	# Require X-fold enrichment over local background to keep the window (similar to MACS)
+	keep <- lapply(filterstat, function(x) {
+		kp <- x$filter > log2(logFC)
+		return(kp)
+	})
+
+	filtered.data <- lapply(keep, function(keep) {
+		return(data[keep,])
+	})
+
+	## merge nearby windows (within 50bp) to get broader TSS
+	merged <- lapply(filtered.data, function(d) {
+		return(csaw::mergeWindows(d, tol = 50L, ignore.strand = FALSE))
+	})
+
+	## write merged output for each group
+	mapply(function(bedfile, group) {
+		rtracklayer::export.bed(object = bedfile, con = group)
+	}, bedfile = merged, group = paste0(outfile_prefix, "_" , design$group, ".bed") )
+
+	## write out the union of merges
+	merged %<>% lapply(function(x) return(x$region))
+	mergedall <- Reduce(union, merged)
+	rtracklayer::export.bed(mergedall,  con = paste(outfile_prefix, "merged.bed", sep = "_"))
+}
