d6cb2c8867f84a78e43d640e05b979f205ea4ec1
vivekbhr <bhardwaj@ie-freiburg.mpg.de>
2018-02-06 17:33:38 +0100

    added new method and generic for plot_TSSprecision, modified function param name to CSobject instead of CapSet with interfered with generic name

---
+++ R/AllClasses.R
@@ -0,0 +1,111 @@
+#' Create a new CapSet object
+#'
+#' @param expMethod experiment method ('CAGE', 'RAMPAGE' or 'MAPCap')
+#' @param fastqType fastq file type. Should be either 'single' or 'paired'
+#' @param fastq_R1 path for Read R1 (or file path for single end reads)
+#' @param fastq_R2 path for Read R2 (for paired end reads)
+#' @param sampleInfo data.frame with sample information, with row.names = barcode sequences,
+#'                   and first column containing corresponding sample names.
+#'
+#' @return An object of class CapSet
+#' @export
+#'
+#' @examples
+#'
+#' \dontrun{
+#' df <- data.frame(row.names = c("TTAGCC" ,"CAAGTG"), samples = c("one", "two") )
+#' cs <- newCapSet(fastqType = "paired",
+#'                 fastq_R1 = "inst/extdata/test_R1.fastq.gz",
+#'                 fastq_R2 = "inst/extdata/test_R2.fastq.gz",
+#'                 expMethod = "MAPCap", sampleInfo = df)
+#'}
+#'
+
+newCapSet <- function(sampleInfo, expMethod, fastqType, fastq_R1, fastq_R2 = NULL) {
+	stopifnot(!(class(sampleInfo) %in% c("data.frame", "DataFrame")))
+	# convert sampleInfo to a DataFrame
+	info <- S4Vectors::DataFrame(sampleInfo)
+	# create an instance of CapSet
+	new("CapSet",
+	    sampleInfo = info,  # sample Information
+	    fastqType = fastqType,
+	    fastq_R1 = fastq_R1,
+	    fastq_R2 = fastq_R2,
+	    trimmed_R1 = NULL,
+	    trimmed_R2 = NULL,
+	    expMethod = expMethod,
+	    tss_detected = NULL)
+}
+
+#' Check capset validity
+#'
+#' @param object capset object
+#'
+#' @return boolean
+#'
+
+check_capSet <- function(object) {
+	errors <- character()
+
+	## extract slots
+	fqtype <- object@fastqType
+	R1 <- object@fastq_R1
+	R2 <- object@fastq_R2
+	exp <- object@expMethod
+	info <- object@sampleInfo
+	tss <- object@tss_detected
+
+	## validate slots
+	# fastq
+	if(!(fqtype %in% c("single", "paired") )) {
+		msg <- paste0("Wrong fastq type : ", fqtype, ". Should be either 'single' or 'paired' ")
+		errors <- c(errors, msg)
+
+	} else if(fqtype == "single" & !file.exists(R1) ) {
+		msg <- paste0("Please specify correct fastq file path for fastq_R1 ")
+		errors <- c(errors, msg)
+
+	} else if(fqtype == "paired" & !(file.exists(R1) | file.exists(R2) ) ) {
+		msg <- paste0("Please specify correct fastq file path for both fastq_R1 and fastq_R2 ")
+		errors <- c(errors, msg)
+	}
+	# experiment
+	if(!(exp %in% c("CAGE", "RAMPAGE", "MAPCap") )) {
+		msg <- paste0("Experiment type should be among : 'CAGE', 'RAMPAGE' or 'MAPCap' ")
+		errors <- c(errors, msg)
+
+	}
+	# sampleInfo
+	if(!(class(info) %in% c("data.frame", "DataFrame")) ) {
+		msg <- paste0("sampleInfo should be a data frame ")
+		errors <- c(errors, msg)
+	}
+        # TSS info
+        if(!(class(tss) %in% c("NULL", "GRangesList")) ) {
+        	msg <- paste0("tss_detected should be a GRangesList object ")
+        	errors <- c(errors, msg)
+        }
+
+	## return
+	if (length(errors) == 0) TRUE else errors
+}
+
+## char or NULL class
+setClassUnion("charOrNULL", c("character", "NULL"))
+
+#' CapSet object
+#'
+#' @rdname newCapSet
+#' @importClassesFrom S4Vectors DataFrame
+#'
+CapSet <- setClass("CapSet",
+		   slots = c(fastqType = "character",
+		   	  fastq_R1 = "character",
+		   	  fastq_R2 = "charOrNULL",
+		   	  trimmed_R1 = "charOrNULL",
+		   	  trimmed_R2 = "charOrNULL",
+		   	  expMethod = "character",
+		   	  sampleInfo = "DataFrame",
+		   	  tss_detected = "ANY"
+		   	  ),
+		   validity = check_capSet)
--- R/plotting_functions.R
+++ R/plotting_functions.R
@@ -1,10 +1,12 @@
 
 #' Plot read statistics from the CapSet object
 #'
-#' @param CapSet The \code{\link{CapSet}} object
-#' @param plotType The type of plot to make. Choose from "numbers" or "proportions". If "proportions"
-#'                 is selected, the proportion of reads w.r.t total demultiplexed reads per sample
-#'                 would be plotted
+#' @param CSobject The \code{\link{CapSet}} object
+#' @param plotType The type of plot to make. Choose from "stack" or "dodge" for either a stacked barchart,
+#'                  or a bar chart with "dodged" positions (analogous to ggplot)
+#' @param plotValue What values to plot. Choose from "numbers" or "proportions". If "proportions"
+#'                  is selected, the proportion of reads w.r.t total demultiplexed reads per sample
+#'                  would be plotted
 #' @param outfile Output file name. (filename extention would be used to determine type).
 #'                If outfile not specified, the plot would be retured on the screen
 #'
@@ -16,9 +18,19 @@
 #' plot_readStats(CapSet = cs, plotType = "numbers")
 #' }
 
-plot_readStats <- function(CapSet, plotType = c("numbers", "proportions"), outfile = NULL) {
-	si <- sampleInfo(CapSet)
+plot_readStats <- function(CSobject,
+			   plotType = c("stack", "dodge"),
+			   plotValue = c("numbers", "proportions"),
+			   outFile = NULL) {
+
+	## evaluate expressions
+	stopifnot(is(CapSet, "CapSet"))
+	stopifnot(plotType %in% c("stack", "dodge"))
+	stopifnot(plotValue %in% c("numbers", "proportions"))
+
 	## get info on how many columns present
+	si <- sampleInfo(CapSet)
+
 	sicols <- colnames(si)
 	fields_toplot <- c("demult_reads", "num_mapped", "num_filtered", "num_intss")
 	msg <- "Plotting following information :"
@@ -29,22 +41,31 @@ plot_readStats <- function(CapSet, plotType = c("numbers", "proportions"), outfi
 	## prepare df
 	si_stats <- data.frame(sample = si$samples,
 			       demutiplexed_reads = si$demult_reads,
-			       mapped_reads = si$num_mapped,
-			       duplicate_free_reads = si$num_filtered,
-			       reads_within_TSS = si$num_intss)
-	if(plotType == "proportions") {
+			       mapped_reads = si$num_mapped)
+	## fill additional cols if present
+	if(!(is.null(si$num_filtered))) si_stats$duplicate_free_reads <- si$num_filtered
+	if(!(is.null(si$num_intss))) si_stats$reads_within_TSS <- si$num_intss
+
+	if(plotValue == "proportions") {
 		si_stats[-1] <- si_stats[-1]/si_stats$demutiplexed_reads
+		# for stacked chart it's important to plot the cumulative difference of the numbers
+		if(plotType == "stack") {
+			si_stats[-1] <- get_stackedNum(si_stats[-1])
+		}
 		y_label <- "Proportion of demultiplexed reads"
 	} else {
+		if(plotType == "stack") {
+			si_stats[-1] <- get_stackedNum(si_stats[-1])
+		}
 		y_label <- "Number of reads"
 	}
 
 	si_stats <- reshape2::melt(si_stats, id.vars = "sample")
 	# plot stacked barchart
 	p <- ggplot(si_stats, aes_string("sample", "value", fill = "variable")) +
-		geom_bar(stat = "identity", position = "stack") +
+		geom_bar(stat = "identity", position = plotType, color = "black") +
 		theme_light(base_size = 16)  +
-		scale_fill_brewer(type = "seq", palette = "Blues") +
+		scale_fill_brewer(type = "seq", palette = "YlGnBu") +
 		coord_flip() +
 		labs(x = "Sample", y = y_label, fill = "Category")
 	# return
@@ -56,9 +77,20 @@ plot_readStats <- function(CapSet, plotType = c("numbers", "proportions"), outfi
 
 }
 
+## get cumulative differences of values from a DF, to plot stacked barchart of numbers
+get_stackedNum <- function(df) {
+	num <- apply(df, 1, function(x) {
+		n <- c(abs(diff(as.numeric(x))), x[length(x)])
+		return(n)
+	})
+	return(as.data.frame(t(num)))
+}
 
 
-#' Plot precision of TSS calling between multiple samples
+#' Compare the precision of TSS detection between multiple samples
+#'
+#' @description Plot precision of TSS detection from multiple samples (bed files) with respect to
+#' a given reference annotation.
 #'
 #' @param TSSbedFiles BED files with TSS called by the CAGE wrapper
 #' @param sampleNames Labels for Input samples
@@ -67,14 +99,18 @@ plot_readStats <- function(CapSet, plotType = c("numbers", "proportions"), outfi
 #' @param outFile Output file name (filename extention would be used to determine type).
 #'                If outfile not specified, the plot would be retured on the screen
 #'
-#' @return A ggplot object, or a file. Plot showing % TSS called per sample w.r.t their distance to TSS
-#' @export
-#' @importFrom ggplot2 aes_string stat_ecdf theme_light scale_x_continuous scale_color_brewer ggsave
+#' @rdname plot_TSSprecision
+#' @return A ggplot object, or a file. Plot showing perent of TSS detected per sample with respect to
+#'         their cumulative distance to TSS of the provided reference
 #'
+#' @export
 #' @examples
 #' \dontrun{
 #' library("TxDb.Dmelanogaster.UCSC.dm6.ensGene")
 #' transcripts <- transcripts(dm6GTF)
+#'
+#' Plotting the precision using a pre computed set of TSS (.bed files) :
+#'
 #' files <- system.file("extdata", "testTSS.bed", package = "mapcapR")
 #' plot_TSSprecision(TSSbedFiles = files, sampleNames = "testTSS",
 #' 		reference = transcripts , distanceCutoff = 500,
@@ -82,15 +118,94 @@ plot_readStats <- function(CapSet, plotType = c("numbers", "proportions"), outfi
 #' }
 
 plot_TSSprecision <- function(TSSbedFiles, sampleNames, reference, distanceCutoff = 500 , outFile = NULL) {
-	# resize gene/transcript file to start
-	refRanges <- GenomicRanges::resize(reference, width = 1, fix = "start")
-	refRanges <- unique(refRanges)
 	# read bed files
 	tssData <- lapply(TSSbedFiles, rtracklayer::import.bed)
 	names(tssData) <- sampleNames
+	# get plot
+	plt <- plotPrecision(ref = reference, tssData = tssData, distCut = distanceCutoff)
+	# output
+	if(!(is.null(outFile))) {
+		ggsave(outFile, plot = plt, dpi = 300)
+	} else {
+		return(plt)
+	}
+}
+
+#' @rdname plot_TSSprecision
+#' @export
+setGeneric("plot_TSSprecision", function(CSobject,...) standardGeneric("plot_TSSprecision"))
+
+#' Compare the precision of TSS detection between multiple samples
+#'
+#' @description Plot precision of TSS detection from multiple samples present within a \
+#' code{\link{CapSet}} object, with respect to a given reference annotation.
+#'
+#' @param object An object of class \code{\link{CapSet}}
+#' @param reference Reference Transcrips/Genes as a \code{\link{GRanges}} object
+#' @param distanceCutoff Maximum distance (in base pairs) from reference TSS to plot
+#' @param outFile Output file name (filename extention would be used to determine type).
+#'                If outfile not specified, the plot would be retured on the screen
+#' @docType methods
+#' @rdname plot_TSSprecision
+#'
+#' @export
+#' @examples
+#' \dontrun{
+#' Plotting the precision using a CapSet object :
+#'
+#' library("TxDb.Dmelanogaster.UCSC.dm6.ensGene")
+#' transcripts <- transcripts(dm6GTF)
+#'
+#' plot_TSSprecision(CapSet = cs, reference = transcripts, outFile = "TSS_detection_precision.png")
+#' }
+#'
+setMethod(plot_TSSprecision,
+	  signature = "CapSet",
+	  definition =
+	  function(CSobject,
+	  	   reference,
+	  	   distanceCutoff = 500,
+	  	   outFile = NULL) {
+
+	# evaluate expressions
+	stopifnot(is(CSobject, "CapSet"))
+	stopifnot(is(reference, "GRanges"))
+
+	# get the data out
+	if(is.null(CSobject@tss_detected)) {
+		stop("CapSet object does not contain the detected TSS information")
+	} else {
+		tssData <- CSobject@tss_detected
+	}
+	# get plot
+	plt <- plotPrecision(ref = reference, tssData = tssData, distCut = distanceCutoff)
+	# output
+	if(!(is.null(outFile))) {
+		ggsave(outFile, plot = plt, dpi = 300)
+	} else {
+		return(plt)
+	}
+
+
+})
+
+#' Plotprecision background script
+#'
+#' @param ref reference GRanges
+#' @param tssData GRangesList object with TSS detected per sample
+#' @param distCut max distance cutoff
+#'
+#' @importFrom ggplot2 aes_string stat_ecdf theme_light scale_x_continuous scale_color_brewer ggsave
+#' @return ggplot object
+#'
+
+plotPrecision <- function(ref, tssData, distCut) {
+	# resize gene/transcript file to start
+	refRanges <- GenomicRanges::resize(ref, width = 1, fix = "start")
+	refRanges <- unique(refRanges)
 
 	# get distances of bed entries from nearest TSS
-	tssdistances <- sapply(tssData, function(x){
+	tssdistances <- lapply(tssData, function(x){
 		y <- GenomicRanges::distanceToNearest(x,refRanges)
 		return(as.data.frame(y)$distance)
 	})
@@ -103,15 +218,12 @@ plot_TSSprecision <- function(TSSbedFiles, sampleNames, reference, distanceCutof
 	p <- ggplot(tssdistances, aes_string("distances", col = "sample")) +
 		stat_ecdf(geom = "step", size = 1) +
 		theme_light(base_size = 14)  +
-		scale_x_continuous(limits = c(0,distanceCutoff)) +
+		scale_x_continuous(limits = c(0,distCut)) +
 		scale_color_brewer(palette = "Set2") +
 		labs(x = "Distances from nearby TSS (in bp)", y = "Cumulative Fraction",
 		     title = "TSS precisions", col = "Category")
-	if(!(is.null(outFile))) {
-		ggsave(outFile, plot = p, dpi = 300)
-	} else {
-		return(p)
-	}
-
+	return(p)
 
 }
+
+
