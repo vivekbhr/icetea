3a0518bdfa4a31dd2274da9ac1cccc84f423f71b
vivekbhr <bhardwaj@ie-freiburg.mpg.de>
2018-02-23 14:20:26 +0100

    creating CSobject is more flexible and the sampleInfo DF doesn't need to be maniputaed from outside

--- R/example_CSobject.R
+++ R/example_CSobject.R
@@ -17,22 +17,19 @@ exampleCSobject <- function(protocol = "MAPCap") {
     num_intss <- c(46, 53, 138, 88)
 
     dir <- system.file("extdata", package = "icetea")
-    si <- data.frame(row.names = idxlist,
-                samples = fnames,
-                demult_reads = demult_reads,
-                demult_R1 = file.path(dir, 'demult_fastq', paste0(fnames, "_R1.fastq.gz")),
-                demult_R2 = file.path(dir, 'demult_fastq', paste0(fnames, "_R2.fastq.gz")),
-                mapped_file = file.path(dir, 'bam', paste0(fnames, ".bam")),
-                num_mapped = num_mapped,
-                filtered_file = file.path(dir, 'filtered_bam', paste0(fnames, ".filtered.bam")),
-                num_filtered = num_filtered,
-                num_intss = num_intss
-                )
-
-     cs <- newCapSet(expMethod = 'MAPCap', fastqType = 'paired',
+     cs <- newCapSet(expMethod = 'MAPCap',
                     fastq_R1 = file.path(dir, 'mapcap_test_R1.fastq.gz'),
                     fastq_R2 = file.path(dir, 'mapcap_test_R2.fastq.gz'),
-                    sampleInfo = si)
+                    idxList = idxlist,
+                    sampleNames = fnames,
+                    demult_R1 = file.path(dir, 'demult_fastq', paste0(fnames, "_R1.fastq.gz")),
+                    demult_R2 = file.path(dir, 'demult_fastq', paste0(fnames, "_R2.fastq.gz")),
+                    mapped_file = file.path(dir, 'bam', paste0(fnames, ".bam")),
+                    filtered_file = file.path(dir, 'filtered_bam', paste0(fnames, ".filtered.bam"))
+                    )
+     si <- sampleInfo(cs)
+     si$num_intss <- num_intss
+     sampleInfo(cs) <- si
      # tss detected
      tss <- load(file.path(dir, "testTSS_grl.Rdata"))
      cs@tss_detected <- tss
